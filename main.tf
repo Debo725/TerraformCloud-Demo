4b165928f056c3584b64ec899a9e8f3bdee94d78
Adebowale Adeleye <aadeleye725@gmail.com>
2023-03-18 17:51:13 -0400

    edited provider file

--- main.tf
+++ main.tf
@@ -10,10 +10,10 @@ terraform {
 
 provider "azurerm" {
 
-subscription_id = var.subscriptionID
-client_id       = var.clientID
-client_secret   = var.clientsecret
-tenant_id       = var.tenantID
+subscriptionid = var.subscriptionID
+clientid       = var.clientID
+clientsecret   = var.clientsecret
+tenantid       = var.tenantID
   features {}
 }
 
